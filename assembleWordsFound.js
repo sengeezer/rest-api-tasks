726b148a9426db344458e69f97ae19f11f0efb0c
Fabio Albertin <albertin@gmail.com>
2016-09-16 01:34:08 +0100

    Switch to async/whilst

--- assembleWordsFound.js
+++ assembleWordsFound.js
@@ -1,5 +1,6 @@
 var Promise = require("bluebird");
-var asyncDuring = require('async/during');
+// var asyncDuring = require('async/during');
+var asyncWhilst = require('async/whilst');
 
 var wordNumber = require('./models/word2');
 
@@ -9,13 +10,11 @@ function asWF (req, cW) {
     var count = 0,
         resk;
 
-    asyncDuring(
-      function (callback) {
-        return callback(null, count < reqSize);
-      },
-
-      function (callback) {
+    asyncWhilst(
+      function() { return count < reqSize; },
+      function(callback) {
         wordNumber(req[count], function(err, verified) {
+          console.log('verified: ' + verified);
           if (!err) {
             if (typeof verified !== 'undefined') {
               cW.push(verified);
@@ -25,10 +24,41 @@ function asWF (req, cW) {
           else if (err) {
             console.log('awf cb err: ' + err);
           }
+
         });
 
         count++;
-        callback();
+        callback(null, count);
+      },
+      function(err, n) {
+        console.log('err: ' + err + ' n: ' + n);
+        return cW;
+
+      }
+    );
+    /*
+    asyncDuring(
+      function (callback) {
+        return callback(null, count < reqSize);
+      },
+
+      function (callback) {
+        wordNumber(req[count], function(err, verified) {
+          console.log('verified: ' + verified);
+          if (!err) {
+            if (typeof verified !== 'undefined') {
+              cW.push(verified);
+            }
+            console.log('confd: ' + verified);
+          }
+          else if (err) {
+            console.log('awf cb err: ' + err);
+          }
+
+        });
+
+        count++;
+        callback(cW);
       },
 
       function (err) {
@@ -39,7 +69,7 @@ function asWF (req, cW) {
         return cW;
       }
     );
-
+*/
 }
 
 module.exports = {
